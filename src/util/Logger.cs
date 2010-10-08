643f7069852915178a27fab1d75e193268de3b36
Igor Russkih <irusskih@gmail.com>
2010-10-08 22:13:01 +0400

    file logger

--- src/util/Logger.cs
+++ src/util/Logger.cs
@@ -1,13 +1,24 @@
 using System;
+using System.IO;
 
 namespace hobd{
 
 public class Logger
 {
 
+    static StreamWriter fs = new StreamWriter(new FileStream("log.txt", FileMode.Append));
+    
     public static void trace(String s)
     {
+        log("TRACE", s);
+    }
+    
+    static void log(string level, string s)
+    {
+        s = "["+level+"] "+DateTime.Now.ToShortTimeString() + ":    " + s;
         System.Console.WriteLine(s);
+        fs.Write(s+"\n");
+        fs.Flush();    
     }
 
 }
