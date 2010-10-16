533a619439c54c9efab72fce7ad05dae25662d56
Igor Russkih <irusskih@gmail.com>
2010-10-17 00:06:27 +0400

    CM

--- copy.bat
+++ copy.bat
@@ -1,2 +1,5 @@
 "C:\Program Files\Windows Mobile Developer Power Toys\CECopy\cecopy.exe" /is bin/debug_ce/hobd.exe dev:\SDMMC\WinCEdev\hobd
 "C:\Program Files\Windows Mobile Developer Power Toys\CECopy\cecopy.exe" /is bin/debug_ce/fleux.dll dev:\SDMMC\WinCEdev\hobd
+
+"C:\Program Files\Windows Mobile Developer Power Toys\CECopy\cecopy.exe" /is bin/debug/default.vehicles dev:\SDMMC\WinCEdev\hobd
+"C:\Program Files\Windows Mobile Developer Power Toys\CECopy\cecopy.exe" /is bin/debug/default-landscape.layout dev:\SDMMC\WinCEdev\hobd
--- src/util/Logger.cs
+++ src/util/Logger.cs
@@ -6,19 +6,32 @@ namespace hobd{
 public class Logger
 {
     public const bool TRACE = true;
+    public const bool ERROR = true;
     
     static StreamWriter fs = new StreamWriter(new FileStream( Path.Combine(HOBD.AppPath, "log.txt"), FileMode.Append));
     
+    public static void error(String comp, String msg, Exception e)
+    {
+        if (ERROR) log("ERROR", comp, msg, e);
+    }
+
     public static void trace(String s)
     {
-        if (TRACE) log("TRACE", s);
+        if (TRACE) log("TRACE", "", s, null);
     }
     
-    static void log(string level, string s)
+    static void log(string level, string comp, string msg, Exception e)
     {
-        s = "["+level+"] "+DateTime.Now.ToLongTimeString() + ":    " + s;
-        System.Console.WriteLine(s);
-        fs.Write(s+"\n");
+        if (comp == null) comp = "";
+        if (msg == null) msg = "";
+        
+        msg = "["+level+"] ["+comp+"] "+DateTime.Now.ToLongTimeString() + ":    " + msg;
+        if (e != null)
+        {
+            msg +=  " " + e.Message + e.StackTrace;
+        }
+        System.Console.WriteLine(msg);
+        fs.Write(msg+"\n");
         fs.Flush();    
     }
 
