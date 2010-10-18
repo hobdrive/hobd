0945594aa212cca952922e9ff144ff40b2a6e701
Igor Russkih <irusskih@gmail.com>
2010-10-19 00:14:35 +0400

    sensorText uses drawCenterText

--- src/ui/SensorTextElement.cs
+++ src/ui/SensorTextElement.cs
@@ -20,6 +20,18 @@ public class SensorTextElement: IUIElement, IDimensionAwareElement
     {
         this.Text = attrs["id"];
         this.Style = HOBD.theme.PhoneTextNormalStyle;
+        string textSize = null;
+        attrs.TryGetValue("size", out textSize);
+        
+        if (textSize == "small")
+            this.Style.FontSize = HOBD.theme.PhoneFontSizeSmall;
+        else if (textSize == "large")
+            this.Style.FontSize = HOBD.theme.PhoneFontSizeLarge;
+        else if (textSize == "huge")
+            this.Style.FontSize = HOBD.theme.PhoneFontSizeExtraLarge;
+        else 
+            this.Style.FontSize = HOBD.theme.PhoneFontSizeNormal;
+            
         //var style = new TextStyle(HOBD.theme.PhoneTextLargeStyle.FontFamily, HOBD.theme.PhoneFontSizeMediumLarge, HOBD.theme.PanoramaNormalBrush);
     }
 
@@ -32,17 +44,17 @@ public class SensorTextElement: IUIElement, IDimensionAwareElement
     public void Draw(IDrawingGraphics drawingGraphics)
     {
         drawingGraphics.Color(Color.FromArgb(100,100,100));
-        //drawingGraphics.DrawRectangle(10, 10, drawingGraphics.Width, drawingGraphics.Bottom);
         drawingGraphics.DrawRectangle(0, 0, width, height);
         
-        drawingGraphics.Style(this.Style);
-        drawingGraphics.MoveTo(2,2).Color(Color.FromArgb(64, 64, 64)).DrawText(this.Text);
-        drawingGraphics.MoveTo(0,0).Style(this.Style).DrawText(this.Text);
+        //drawingGraphics.Style(this.Style);
+        //drawingGraphics.MoveTo(2,2).Color(Color.FromArgb(64, 64, 64)).DrawText(this.Text);
+        drawingGraphics.MoveTo(0,0).Style(this.Style).DrawCenterText(this.Text, width, height);
     }
 
     public void HandleTap(System.Drawing.Point point)
     {
-    	//drawingGraphics.Style(this.Style).Bold(true).DrawText(this.text + n);
+    	   
+        //drawingGraphics.Style(this.Style).Bold(true).DrawText(this.text + n);
         if (this.HandleTapAction != null)
         {
             this.HandleTapAction();
