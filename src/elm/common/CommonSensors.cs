fb906c5a58edd55ef173998d657de844615b911f
Igor Russkih <irusskih@gmail.com>
2010-10-23 23:00:54 +0400

    distance common sensor

---
+++ src/elm/common/CommonSensors.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace hobd
+{
+    
+public class CommonSensors : SensorProvider
+{
+
+    public CommonSensors()
+    {
+    }
+    
+    public string GetName()
+    {
+        return "CommonSensors";
+    }
+
+    public string GetDescription()
+    {
+        return "Common derived data sensors";
+    }
+    
+    public string GetDescription(string lang)
+    {
+        return GetDescription();
+    }
+
+    public void Activate(SensorRegistry registry)
+    {
+        registry.Add(new DistanceSensor());
+    }
+}
+}
---
+++ src/elm/common/DistanceSensor.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace hobd
+{
+
+/**
+ * Calculates total run distance based on speed interpolation
+ */
+public class DistanceSensor : CoreSensor, IAccumulatorSensor
+{
+    public static string MeterUnits = "m.";
+    DateTime prevStamp;
+    bool firstRun = true;
+        
+    public DistanceSensor()
+    {
+        ID = "DistanceRun";
+        Name = "DistanceRun";
+        Description = "Total run distance";
+        Units = DistanceSensor.MeterUnits;
+    }
+
+    public override void SetRegistry(SensorRegistry registry)
+    {
+        base.SetRegistry(registry);
+        registry.AddListener("Speed", OnSpeedChange);
+    }
+    
+    public void OnSpeedChange(Sensor speed)
+    {
+        TimeStamp = speed.TimeStamp;
+        if (firstRun){
+            prevStamp = TimeStamp;
+            firstRun = false;
+            return;
+        }
+        Value += speed.Value * 1000 / 3600 * (TimeStamp-prevStamp).Milliseconds / 1000;
+        prevStamp = TimeStamp;
+        registry.TriggerListeners(this);
+    }
+    
+    public virtual void Reset()
+    {
+        Value = 0;
+        firstRun = true;
+    }
+    public virtual void Suspend()
+    {
+        firstRun = true;
+    }
+        
+}
+
+}
