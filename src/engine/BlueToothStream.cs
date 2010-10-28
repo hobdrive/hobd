f9c87b1093f573b218adffe83c2b00b5d161a235
Igor Russkih <irusskih@gmail.com>
2010-10-28 16:15:32 +0400

    BT url parsing

--- src/engine/BlueToothStream.cs
+++ src/engine/BlueToothStream.cs
@@ -2,6 +2,7 @@ using System;
 using System.Text;
 using System.Collections;
 using System.Net.Sockets;
+using System.Text.RegularExpressions;
 using InTheHand.Net;
 using InTheHand.Net.Sockets;
 using InTheHand.Net.Bluetooth;
@@ -18,81 +19,112 @@ public class BluetoothStream: IStream
     public BluetoothStream()
     {}
 
+    /**
+     * returns string array of {address, serviceid, pin}
+     */
+    public static string[] ParseUrl(string url)
+    {
+        Regex rx = new Regex(@"^btspp:// ([\d\w]+) (\:(\d+))? (\;pin=(\d+))? $", RegexOptions.IgnorePatternWhitespace);
+        
+        var match = rx.Match(url);
+        
+        if (match == null)
+            return new string[]{null, null, null};
+        
+        var serviceid = match.Groups[3].Value;
+        if (serviceid == "") serviceid = null;
+
+        var pid = match.Groups[5].Value;
+        if (pid == "") pid = null;
+
+        return new string[]{ match.Groups[1].Value, serviceid, pid };
+    }
+    
     public void Open(String url)
     {
-		try{
-			BluetoothAddress address = BluetoothAddress.Parse(url);
-			bluetoothClient = new BluetoothClient();
-			bluetoothClient.SetPin(address, "0000");
-			BluetoothEndPoint btep = new BluetoothEndPoint(address, BluetoothService.SerialPort, 1);
-			bluetoothClient.Connect(btep);
-			stream = bluetoothClient.GetStream();
-			if (stream == null){
-				bluetoothClient.Close();
-				bluetoothClient = null;
-			}else{
-				if (stream.CanTimeout){
-					stream.WriteTimeout = 2;
-					stream.ReadTimeout = 2;
-				}
-			}
-		}catch(System.IO.IOException){
-			bluetoothClient = null;
-		}
+        try{
+            
+            var parsed_url = ParseUrl(url);
+            Logger.trace("BluetoothStream", "Open " + parsed_url[0] + " serviceid " + parsed_url[1] + " pin " + parsed_url[2]);
+
+            BluetoothAddress address = BluetoothAddress.Parse(parsed_url[0]);
+
+            bluetoothClient = new BluetoothClient();
+            if (parsed_url[2] != null)
+                bluetoothClient.SetPin(address, parsed_url[2]);
+            BluetoothEndPoint btep;
+            if (parsed_url[1] != null)
+                btep = new BluetoothEndPoint(address, BluetoothService.SerialPort, int.Parse(parsed_url[1]));
+            else
+                btep = new BluetoothEndPoint(address, BluetoothService.SerialPort);
+            bluetoothClient.Connect(btep);
+            stream = bluetoothClient.GetStream();
+            if (stream == null){
+                bluetoothClient.Close();
+                bluetoothClient = null;
+            }else{
+                if (stream.CanTimeout){
+                    stream.WriteTimeout = 2;
+                    stream.ReadTimeout = 2;
+                }
+            }
+        }catch(System.IO.IOException){
+            bluetoothClient = null;
+        }
     }
     
     public void Close()
     {
-		if (bluetoothClient != null)
-		{
-			bluetoothClient.Close();
-			bluetoothClient = null;
-		}
+        if (bluetoothClient != null)
+        {
+            bluetoothClient.Close();
+            bluetoothClient = null;
+        }
 }
     
     public bool HasData()
     {
-		if (bluetoothClient != null && stream != null)
-		{
-			try{
-				return stream.DataAvailable;
-			}catch(System.IO.IOException){
-				bluetoothClient = null;
-				return false;
-			}
-		}
-		    return false;
+        if (bluetoothClient != null && stream != null)
+        {
+            try{
+                return stream.DataAvailable;
+            }catch(System.IO.IOException){
+                bluetoothClient = null;
+                return false;
+            }
+        }
+            return false;
     }
     
     public byte[] Read()
     {
-		if (bluetoothClient != null)
-		{
-			byte[] buf = new byte[1024];
-			int len;
-			try{
-				len = stream.Read(buf, 0, buf.Length);
-			}catch(System.IO.IOException){
-				bluetoothClient = null;
-				return null;
-			}
-			byte[] outputData_ = new byte[len];
-			Array.Copy(buf, 0, outputData_, 0, len);
-			return outputData_;
-		}
-		return null;
+        if (bluetoothClient != null)
+        {
+            byte[] buf = new byte[1024];
+            int len;
+            try{
+                len = stream.Read(buf, 0, buf.Length);
+            }catch(System.IO.IOException){
+                bluetoothClient = null;
+                return null;
+            }
+            byte[] outputData_ = new byte[len];
+            Array.Copy(buf, 0, outputData_, 0, len);
+            return outputData_;
+        }
+        return null;
     }
     
     public void Write(byte[] array, int offset, int length)
     {
-		if (bluetoothClient != null && stream != null)
-		{
-			try{
-				stream.Write(array, offset, length);
-			}catch(System.IO.IOException){
-				bluetoothClient = null;
-			}
-		}
+        if (bluetoothClient != null && stream != null)
+        {
+            try{
+                stream.Write(array, offset, length);
+            }catch(System.IO.IOException){
+                bluetoothClient = null;
+            }
+        }
     }
 }
 }
