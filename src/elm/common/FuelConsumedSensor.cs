2f81f54217f7cd84a2229f5251e864c7f599d4d0
Igor Russkih <irusskih@gmail.com>
2010-11-01 17:43:48 +0300

    fuel consumed sensor

---
+++ src/elm/common/FuelConsumedSensor.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace hobd
+{
+
+/**
+ * Calculates total fuel consumed during the trip
+ * Uses the underlying Liters Per Hour sensor
+ */
+public class FuelConsumedSensor : CoreSensor, IAccumulatorSensor
+{
+    public static string VolumeUnits = "l.";
+    long prevStamp;
+    bool firstRun = true;
+
+    public int ListenInterval{get; set;}
+        
+    public FuelConsumedSensor()
+    {
+        ID = "FuelConsumed";
+        Name = "Fuel Consumed";
+        Description = "Total fuel consumed";
+        Units = FuelConsumedSensor.VolumeUnits;
+        ListenInterval = 2000;
+    }
+
+    public override void SetRegistry(SensorRegistry registry)
+    {
+        base.SetRegistry(registry);
+        registry.AddListener("LitersPerHour", OnChange, ListenInterval);
+    }
+
+    void OnChange(Sensor lph)
+    {
+        TimeStamp = lph.TimeStamp;
+        if (firstRun){
+            prevStamp = TimeStamp;
+            firstRun = false;
+            return;
+        }
+        Value += lph.Value / 3600 * (TimeStamp-prevStamp) / 1000;
+        prevStamp = TimeStamp;
+        registry.TriggerListeners(this);
+    }
+    
+    public virtual void Reset()
+    {
+        Value = 0;
+        firstRun = true;
+    }
+    public virtual void Suspend()
+    {
+        firstRun = true;
+    }
+
+}
+
+}
