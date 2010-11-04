92a761f339143a33e682125887563812d804e1f0
Igor Russkih <irusskih@gmail.com>
2010-11-05 02:06:45 +0300

    huge Speed

--- build.bat
+++ build.bat
@@ -1,2 +1,5 @@
+copy config\default* bin\Debug\
+copy config\*.lang bin\Debug\
+
 c:\WINDOWS\Microsoft.NET\Framework\v3.5\MSBuild.exe /p:Platform=AnyCPU hobd.csproj
 c:\WINDOWS\Microsoft.NET\Framework\v3.5\MSBuild.exe /p:Platform=WINCE hobd.csproj
