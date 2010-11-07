4384fe30b09a3bbd13660aec36d49ce64e6c6a61
Igor Russkih <irusskih@gmail.com>
2010-11-08 00:34:20 +0300

    readme

--- copy.bat
+++ copy.bat
@@ -1,8 +1,8 @@
 "C:\Program Files\Windows Mobile Developer Power Toys\CECopy\cecopy.exe" /is bin/debug_ce/hobd.exe dev:\SDMMC\WinCEdev\hobd
 "C:\Program Files\Windows Mobile Developer Power Toys\CECopy\cecopy.exe" /is bin/debug_ce/fleux.dll dev:\SDMMC\WinCEdev\hobd
 
-"C:\Program Files\Windows Mobile Developer Power Toys\CECopy\cecopy.exe" /is bin/debug/default.vehicles dev:\SDMMC\WinCEdev\hobd
-"C:\Program Files\Windows Mobile Developer Power Toys\CECopy\cecopy.exe" /is bin/debug/default-landscape.layout dev:\SDMMC\WinCEdev\hobd
+"C:\Program Files\Windows Mobile Developer Power Toys\CECopy\cecopy.exe" /is config/default.vehicles dev:\SDMMC\WinCEdev\hobd
+"C:\Program Files\Windows Mobile Developer Power Toys\CECopy\cecopy.exe" /is config/default-landscape.layout dev:\SDMMC\WinCEdev\hobd
 
 "C:\Program Files\Windows Mobile Developer Power Toys\CECopy\cecopy.exe" /is config/ru.lang dev:\SDMMC\WinCEdev\hobd
 "C:\Program Files\Windows Mobile Developer Power Toys\CECopy\cecopy.exe" /is config/en.lang dev:\SDMMC\WinCEdev\hobd
