6da88d1c70d2f061ded95ea5f78230d3b27e8bf1
Igor Russkih <irusskih@gmail.com>
2010-11-08 10:18:41 +0300

    CM stuff

--- distr.bat
+++ distr.bat
@@ -1,5 +1,7 @@
 @echo off
 
+set Z="C:\Program Files\7-Zip\7z.exe"
+
 set VERSION=0.2
 
 FOR /F "tokens=*" %%i in ('DATE /T') do SET DATE=%%i 
@@ -21,18 +23,20 @@ c:\WINDOWS\Microsoft.NET\Framework\v3.5\MSBuild.exe /p:Platform=AnyCPU /p:Config
 c:\WINDOWS\Microsoft.NET\Framework\v3.5\MSBuild.exe /p:Platform=WINCE /p:Configuration=Release hobd.csproj
 :skip
 
-set Z="C:\Program Files\7-Zip\7z.exe"
-
 mkdir distr
+del /F /Q distr\win32
+del /F /Q distr\wince
 mkdir distr\win32
+mkdir distr\win32\sensors
 mkdir distr\wince
+mkdir distr\wince\sensors
 
 copy bin\Release\hobd.exe distr\win32
 copy bin\Release\Fleux.dll distr\win32
 copy lib\win32\InTheHand.Net.Personal.dll distr\win32
 copy config\default* distr\win32
 copy config\*.lang distr\win32
-copy src\elm\gen\*.ecuxml distr\win32
+copy src\elm\gen\*.ecuxml distr\win32\sensors
 copy README.MD distr\win32
 del distr\win32\log.txt
 del distr\win32\config.xml
@@ -41,12 +45,14 @@ copy bin\Release_ce\hobd.exe distr\wince
 copy bin\Release_ce\Fleux.dll distr\wince
 copy lib\wince\InTheHand.Net.Personal.dll distr\wince
 copy config\default* distr\wince
-copy src\elm\gen\*.ecuxml distr\wince
+copy src\elm\gen\*.ecuxml distr\wince\sensors
 copy config\*.lang distr\wince
 copy README.MD distr\wince
 del distr\wince\log.txt
 del distr\wince\config.xml
 
+del distr\hobd-%VERSION%.win32.zip
+del distr\hobd-%VERSION%.wince.zip
 
 %Z% a distr\hobd-%VERSION%.win32.zip .\distr\win32\*
 %Z% a distr\hobd-%VERSION%.wince.zip .\distr\wince\*
--- src/HOBDBuild.cs
+++ src/HOBDBuild.cs
@@ -4,6 +4,6 @@ namespace hobd
 sealed class HOBDBuild 
 { 
     public const string Version   = "0.2"; 
-    public const string TimeStamp = "08.11.2010  09:29 "; 
+    public const string TimeStamp = "08.11.2010  10:15 "; 
 } 
 } 
