548137773a1df0f0fa64dbe055ba209d6e8abce7
Igor Russkih <irusskih@gmail.com>
2010-11-11 00:55:04 +0300

    make Theme return singletone style objects

--- src/HOBDTheme.cs
+++ src/HOBDTheme.cs
@@ -1,82 +1,42 @@
-﻿using Fleux.Controls.List;
+﻿using System;
+using System.Drawing;
+using Fleux.Styles;
+using Fleux.Controls.List;
 using Fleux.Core.GraphicsHelpers;
 
 namespace hobd
 {
 
-using System;
-using System.Drawing;
-using Fleux.Styles;
 
-/// <summary>
-/// Use as reference http://msdn.microsoft.com/en-us/library/ff769552(VS.92).aspx
-/// </summary>
-[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.StyleCop.CSharp.OrderingRules", "SA1201:ElementsMustAppearInTheCorrectOrder",
-    Justification = "Reviewed. Suppression is OK here.")]
 public class HOBDTheme
 {
     #region Brushes/Colors
 
     // Foreground color to single-out items of interest
-    public virtual Color PhoneAccentBrush
-    {
-        get { return Color.FromArgb(40, 160, 220); }
-    }
+    public Color PhoneAccentBrush = Color.FromArgb(40, 160, 220);
 
-    public virtual Color PhoneForegroundBrush
-    {
-        get { return Color.White; }
-    }
+    public Color PhoneForegroundBrush = Color.White;
 
-    public virtual Color PhoneBackgroundBrush
-    {
-        get { return Color.Black; }
-    }
+    public Color PhoneBackgroundBrush = Color.Black;
 
-    public virtual Color PhoneInactiveBrush
-    {
-        get { return Color.Black; }
-    }
+    public Color PhoneInactiveBrush = Color.Black;
 
-    public virtual Color PhoneTextBoxBrush
-    {
-        get { return Color.White; }
-    }
+    public Color PhoneTextBoxBrush = Color.White;
 
-    public virtual Color PhoneSubtleBrush
-    {
-        get { return Color.FromArgb( unchecked( (int)0xA0706060 )); }
-    }
+    public Color PhoneSubtleBrush = Color.FromArgb( unchecked( (int)0xA0706060 ));
 
-    public virtual Color PhoneContrastForegroundBrush
-    {
-        get { return Color.FromArgb( unchecked( (int)0xFFFFF0F0 )); }
-    }
+    public Color PhoneContrastForegroundBrush = Color.FromArgb( unchecked( (int)0xFFFFF0F0 ));
+
+    public Color PanoramaNormalBrush = Color.FromArgb(255, 255, 255);
 
     #endregion
 
     #region Font Families
 
-    // TODO: public virtual string PhoneFontFamilyNormal { get { return "Segoe WP"; } }
-    public virtual string PhoneFontFamilyNormal
-    {
-        get { return "Segoe WP SemiLight"; }
-    }
-
-    public virtual string PhoneFontFamilyLight
-    {
-        get { return "Segoe WP Light"; }
-    }
-
-    public virtual string PhoneFontFamilySemiLight
-    {
-        get { return "Segoe WP SemiLight"; }
-    }
-
-    public virtual string PhoneFontFamilySemiBold
-    {
-        get { return "Segoe WP Semibold"; }
-    }
+    public string PhoneFontFamilyNormal = "Segoe WP SemiLight";
+    public string PhoneFontFamilyLight = "Segoe WP Light";
+    public string PhoneFontFamilySemiLight = "Segoe WP SemiLight";
+    public string PhoneFontFamilySemiBold = "Segoe WP Semibold";
 
     #endregion
 
@@ -91,262 +51,131 @@ public class HOBDTheme
     ////public virtual int PhoneFontSizeExtraExtraLarge { get { return 54; } }
     ////public virtual int PhoneFontSizeHuge { get { return 140; } }
 
-    public virtual int PhoneFontSizeSmall
-    {
-        get { return 9; }
-    }
+    public int PhoneFontSizeSmall = 9;
 
-    public virtual int PhoneFontSizeNormal
-    {
-        get { return 12; }
-    }
+    public int PhoneFontSizeNormal = 12;
 
-    public virtual int PhoneFontSizeMedium
-    {
-        get { return 13; }
-    }
+    public int PhoneFontSizeMedium = 13;
 
-    public virtual int PhoneFontSizeMediumLarge
-    {
-        get { return 16; }
-    }
+    public int PhoneFontSizeMediumLarge = 16;
 
-    public virtual int PhoneFontSizeLarge
-    {
-        get { return 19; }
-    }
+    public int PhoneFontSizeLarge = 19;
 
-    public virtual int PhoneFontSizeExtraLarge
-    {
-        get { return 30; }
-    }
+    public int PhoneFontSizeExtraLarge = 30;
 
-    public virtual int PhoneFontSizeExtraExtraLarge
-    {
-        get { return 40; }
-    }
+    public int PhoneFontSizeExtraExtraLarge = 40;
 
-    public virtual int PhoneFontSizeHuge
-    {
-        get { return 60; }
-    }
+    public int PhoneFontSizeHuge = 60;
 
     #endregion
+    
+    public ThicknessStyle PhoneHorizontalMargin = new ThicknessStyle(12, 0, 0);
 
-    #region Thickness
+    public ThicknessStyle PhoneVerticalMargin = new ThicknessStyle(0, 12, 0);
 
-    public virtual ThicknessStyle PhoneHorizontalMargin
+    public ThicknessStyle PhoneMargin = new ThicknessStyle(12, 0, 0);
+
+    public ThicknessStyle PhoneTouchTargetOverhang = new ThicknessStyle(12, 0, 0);
+
+    public ThicknessStyle PhoneTouchTargetLargeOverhang = new ThicknessStyle(12, 20, 0);
+
+    public ThicknessStyle PhoneBorderThickness = new ThicknessStyle(3, 0, 0);
+
+    public ThicknessStyle PhoneStrokeThickness = new ThicknessStyle(3, 0, 0);
+
+
+    public TextStyle PhoneTextBlockBase;
+    public TextStyle PhoneTextNormalStyle;
+    public TextStyle PhoneTextTitle1Style;
+    public TextStyle PhoneTextTitle2Style;
+    public TextStyle PhoneTextTitle3Style;
+    public TextStyle PhoneTextLargeStyle;
+    public TextStyle PhoneTextExtraLargeStyle;
+    public TextStyle PhoneTextGroupHeaderStyle;
+    public TextStyle PhoneTextSmallStyle;
+    public TextStyle PhoneTextContrastStyle;
+    public TextStyle PhoneTextAccentStyle;
+    public TextStyle PhoneTextPageTitleStyle;
+    public TextStyle PhoneTextPageSubTitleStyle;
+    public TextStyle PhoneTextPanoramaTitleStyle;
+    public TextStyle PhoneTextPanoramaSubTitleStyle;
+    public TextStyle PhoneTextPanoramaSectionTitleStyle;
+    public TextStyle PhoneTextStatusStyle;
+    public TextStyle PhoneTextSensorDescrStyle;
+
+    public HOBDTheme()
     {
-        get { return new ThicknessStyle(12, 0, 0); }
-    }
-
-    public virtual ThicknessStyle PhoneVerticalMargin
-    {
-        get { return new ThicknessStyle(0, 12, 0); }
-    }
-
-    public virtual ThicknessStyle PhoneMargin
-    {
-        get { return new ThicknessStyle(12, 0, 0); }
-    }
-
-    public virtual ThicknessStyle PhoneTouchTargetOverhang
-    {
-        get { return new ThicknessStyle(12, 0, 0); }
-    }
-
-    public virtual ThicknessStyle PhoneTouchTargetLargeOverhang
-    {
-        get { return new ThicknessStyle(12, 20, 0); }
-    }
-
-    public virtual ThicknessStyle PhoneBorderThickness
-    {
-        get { return new ThicknessStyle(3, 0, 0); }
-    }
-
-    public virtual ThicknessStyle PhoneStrokeThickness
-    {
-        get { return new ThicknessStyle(3, 0, 0); }
-    }
-
-    #endregion
-
-    #region TextStyles
-
-    public virtual TextStyle PhoneTextBlockBase
-    {
-        get
-        {
-            return new TextStyle(
+        PhoneTextBlockBase = new TextStyle(
                 this.PhoneFontFamilyNormal,
                 this.PhoneFontSizeSmall,
                 this.PhoneTextBoxBrush,
                 this.PhoneHorizontalMargin);
-        }
-    }
 
-    public virtual TextStyle PhoneTextNormalStyle
-    {
-        get
-        {
-            return new TextStyle(
+        PhoneTextNormalStyle = new TextStyle(
                 this.PhoneFontFamilyNormal,
                 this.PhoneFontSizeNormal,
                 this.PhoneForegroundBrush);
-        }
-    }
 
-    public virtual TextStyle PhoneTextTitle1Style
-    {
-        get
-        {
-            return new TextStyle(
+        PhoneTextTitle1Style =  new TextStyle(
                 this.PhoneFontFamilySemiLight,
                 this.PhoneFontSizeExtraExtraLarge,
                 this.PhoneForegroundBrush);
-        }
-    }
 
-    public virtual TextStyle PhoneTextTitle2Style
-    {
-        get
-        {
-            return new TextStyle(
+        PhoneTextTitle2Style = new TextStyle(
                 this.PhoneFontFamilySemiLight,
                 this.PhoneFontSizeLarge,
                 this.PhoneForegroundBrush);
-        }
-    }
 
-    public virtual TextStyle PhoneTextTitle3Style
-    {
-        get
-        {
-            return new TextStyle(
+        PhoneTextTitle3Style = new TextStyle(
                 this.PhoneFontFamilySemiLight,
                 this.PhoneFontSizeMedium,
                 this.PhoneForegroundBrush);
-        }
-    }
 
-    public virtual TextStyle PhoneTextLargeStyle
-    {
-        get
-        {
-            return new TextStyle(
+        PhoneTextLargeStyle = new TextStyle(
                 this.PhoneFontFamilySemiLight,
                 this.PhoneFontSizeLarge,
                 this.PhoneForegroundBrush);
-        }
-    }
 
-    public virtual TextStyle PhoneTextExtraLargeStyle
-    {
-        get
-        {
-            return new TextStyle(
+        PhoneTextExtraLargeStyle = new TextStyle(
                 this.PhoneFontFamilySemiLight,
                 this.PhoneFontSizeExtraLarge,
                 this.PhoneForegroundBrush);
-        }
-    }
 
-    public virtual TextStyle PhoneTextGroupHeaderStyle
-    {
-        get
-        {
-            return new TextStyle(
+        PhoneTextGroupHeaderStyle = new TextStyle(
                 this.PhoneFontFamilySemiLight,
                 this.PhoneFontSizeLarge,
                 this.PhoneSubtleBrush);
-        }
-    }
 
-    public virtual TextStyle PhoneTextSmallStyle
-    {
-        get
-        {
-            return new TextStyle(
+        PhoneTextSmallStyle = new TextStyle(
                 this.PhoneFontFamilyNormal,
                 this.PhoneFontSizeSmall,
                 this.PhoneSubtleBrush);
-        }
-    }
 
-    public virtual TextStyle PhoneTextContrastStyle
-    {
-        get
-        {
-            return new TextStyle(
+        PhoneTextContrastStyle = new TextStyle(
                 this.PhoneFontFamilySemiBold,
                 this.PhoneFontSizeNormal,
                 this.PhoneContrastForegroundBrush);
-        }
-    }
 
-    public virtual TextStyle PhoneTextAccentStyle
-    {
-        get
-        {
-            return new TextStyle(
+        PhoneTextAccentStyle = new TextStyle(
                 this.PhoneFontFamilySemiBold,
                 this.PhoneFontSizeNormal,
                 this.PhoneAccentBrush);
-        }
-    }
 
-    #endregion
+        PhoneTextPageTitleStyle = new TextStyle(this.PhoneFontFamilyLight, PhoneFontSizeExtraExtraLarge, this.PhoneSubtleBrush);
 
-    #region additional text styles
+        PhoneTextPageSubTitleStyle = new TextStyle(this.PhoneFontFamilyLight, this.PhoneFontSizeLarge, this.PhoneForegroundBrush);
 
-    public virtual TextStyle PhoneTextPageTitleStyle
-    {
-        get{ return new TextStyle(this.PhoneFontFamilyLight, PhoneFontSizeExtraExtraLarge, this.PhoneSubtleBrush); }
-    }
-
-    public virtual TextStyle PhoneTextPageSubTitleStyle
-    {
-        get{ return new TextStyle(this.PhoneFontFamilyLight, this.PhoneFontSizeLarge, this.PhoneForegroundBrush); }
-    }
-
-    #endregion
-
-    #region Panorama
-
-    public virtual TextStyle PhoneTextPanoramaTitleStyle
-    {
-        get{ return new TextStyle(this.PhoneFontFamilyLight, 65, this.PhoneSubtleBrush); }
-    }
+        PhoneTextPanoramaTitleStyle = new TextStyle(this.PhoneFontFamilyLight, 65, this.PhoneSubtleBrush);
     
-    public virtual TextStyle PhoneTextPanoramaSubTitleStyle
-    {
-        get{ return new TextStyle(this.PhoneFontFamilyLight, PhoneFontSizeExtraLarge, this.PhoneSubtleBrush); }
-    }
+        PhoneTextPanoramaSubTitleStyle = new TextStyle(this.PhoneFontFamilyLight, PhoneFontSizeExtraLarge, this.PhoneSubtleBrush);
+
+        PhoneTextPanoramaSectionTitleStyle = new TextStyle(this.PhoneFontFamilyLight, this.PhoneFontSizeExtraLarge, this.PhoneForegroundBrush);
     
+        PhoneTextStatusStyle = new TextStyle(this.PhoneFontFamilyLight, this.PhoneFontSizeSmall, this.PhoneSubtleBrush);
 
-    public virtual TextStyle PhoneTextPanoramaSectionTitleStyle
-    {
-        get{ return new TextStyle(this.PhoneFontFamilyLight, this.PhoneFontSizeExtraLarge, this.PhoneForegroundBrush); }
+        PhoneTextSensorDescrStyle = new TextStyle(this.PhoneFontFamilyLight, this.PhoneFontSizeSmall, Color.FromArgb( unchecked( (int)0xC0A09090 )));
+
     }
-    
-    public virtual TextStyle PhoneTextStatusStyle
-    {
-        get{ return new TextStyle(this.PhoneFontFamilyLight, this.PhoneFontSizeSmall, this.PhoneSubtleBrush); }
-    }
-
-    public virtual TextStyle PhoneTextSensorDescrStyle
-    {
-        get{ return new TextStyle(this.PhoneFontFamilyLight, this.PhoneFontSizeSmall, Color.FromArgb( unchecked( (int)0xC0A09090 ))); }
-    }
-
-
-    public virtual Color PanoramaNormalBrush
-    {
-        get { return Color.FromArgb(255, 255, 255); }   
-    }
-
-    #endregion
 }
 
 }
