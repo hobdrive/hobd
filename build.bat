6981c7e3e81e79a3b50ae524d4840e90a6849f27
Igor Russkih <irusskih@gmail.com>
2010-11-15 23:41:48 +0300

    CM

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
 copy config\*.lang bin\Debug\
 
 c:\WINDOWS\Microsoft.NET\Framework\v3.5\MSBuild.exe /p:Platform=AnyCPU hobd.csproj
-c:\WINDOWS\Microsoft.NET\Framework\v3.5\MSBuild.exe /p:Platform=WINCE hobd.csproj
+copy lib\win32\InTheHand.Net.Personal.dll bin\Debug\
+rem c:\WINDOWS\Microsoft.NET\Framework\v3.5\MSBuild.exe /p:Platform=WINCE hobd.csproj
