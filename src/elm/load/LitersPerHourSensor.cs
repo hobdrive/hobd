4554580967ad8108e49a7b14df3cf815dc520b50
Igor <igor@hobdrive.com>
2011-05-05 10:01:59 +0400

    engine load based consumption with rpm() function coeff

--- src/elm/load/LitersPerHourSensor.cs
+++ src/elm/load/LitersPerHourSensor.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using hobd;
 
 namespace hobd.elm.load
@@ -10,9 +11,12 @@ public class LitersPerHourSensor : CoreSensor
 {
     public int ListenInterval{get; set;}
 
-    double load_consumption_coeff = 14.7;
+    double load_consumption_coeff = 0.02;
 
-    Sensor load;
+    double[] rpm_matrix = null;
+    double rpm_step;
+
+    Sensor load, rpm;
 
     public LitersPerHourSensor()
     {
@@ -28,12 +32,25 @@ public class LitersPerHourSensor : CoreSensor
         }catch(Exception e){
             Logger.info("LitersPerHourSensor", "Using default coefficient", e);
         }
+        if (registry.VehicleParameters.ContainsKey("rpm-consumption-coeff"))
+        {
+            rpm_matrix = registry.VehicleParameters["rpm-consumption-coeff"]
+                      .Split(new char[]{',', ' '})
+                      .Where(v => v != "")
+                      .Select(v => double.Parse(v, UnitsConverter.DefaultNumberFormat)).ToArray();
+            rpm_step = 10000 / rpm_matrix.Length;
+        }
     }
 
     public override void Activate()
     {
         load = registry.Sensor(OBD2Sensors.EngineLoad);
         registry.AddListener(load, OnSensorChange, ListenInterval);
+        if (rpm_matrix != null)
+        {
+            rpm = registry.Sensor(OBD2Sensors.RPM);
+            registry.AddListener(rpm, OnSensorChange, ListenInterval);
+        }
     }
     
     public override void Deactivate()
@@ -44,7 +61,14 @@ public class LitersPerHourSensor : CoreSensor
     public void OnSensorChange(Sensor s)
     {
         TimeStamp = s.TimeStamp;
-        Value = load.Value * load_consumption_coeff;
+        if (rpm_matrix != null)
+        {
+            int lowidx = (int) (rpm.Value / rpm_step);
+            int nextidx = rpm_matrix.Length-1 == lowidx ? lowidx : lowidx + 1;
+            var coeff = rpm_matrix[lowidx] +  (rpm_matrix[nextidx]-rpm_matrix[lowidx]) * (rpm.Value - lowidx*rpm_step) / rpm_step;
+            Value = load.Value * coeff;
+        }else
+            Value = load.Value * load_consumption_coeff;
         registry.TriggerListeners(this);
     }
 
