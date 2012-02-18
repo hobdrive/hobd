a5c5da415200dc0629b527c4b7e80d80aeff33fd
Igor <igor@hobdrive.com>
2012-02-18 12:44:22 +0400

    cleanup

--- src/elm/ClearDTCSensor.cs
+++ src/elm/ClearDTCSensor.cs
@@ -11,17 +11,13 @@ public class ClearDTCSensor : OBD2Sensor
 {
     public const int ReadInterval = 500 * 60 * 1000;
 
-    public bool Active{ get; set; }
-
     public ClearDTCSensor()
     {
-        Active = false;
         RawCommand = "04";
     }
 
     public override bool SetValue(byte[] dataraw)
     {
-        Active = false;
         this.TimeStamp = DateTimeMs.Now;
         registry.TriggerListeners(this);
         return true;
