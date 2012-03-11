e467c146c96d73833051d649a19e4f989be5054d
Igor <igor@hobdrive.com>
2012-03-11 08:35:40 +0400

    no exceptions in network.

--- src/NetworkHelper.cs
+++ src/NetworkHelper.cs
@@ -21,8 +21,6 @@ public class NetworkHelper
 
     public static Result SendGet(string url)
     {
-        try{
-
         HttpWebRequest wr = (HttpWebRequest)WebRequest.Create(url);
         wr.Method = "GET";
         using(var response = wr.GetResponse() as HttpWebResponse)
@@ -31,10 +29,6 @@ public class NetworkHelper
             {
                 return new Result(Result.RESULT_OK, (int)response.StatusCode, sr.ReadToEnd());
             }
-        }
-
-        }catch(Exception e){
-            return new Result(Result.RESULT_ERR, 0, e.Message);
         }
     }
 
@@ -45,8 +39,6 @@ public class NetworkHelper
 
     public static Result SendPost(string url, Stream body)
     {
-        try{
-
         HttpWebRequest wr = (HttpWebRequest)WebRequest.Create(url);
         wr.Method = "POST";
         wr.ContentType = "application/x-www-form-urlencoded";
@@ -65,10 +57,6 @@ public class NetworkHelper
             {
                 return new Result(Result.RESULT_OK, (int)response.StatusCode, sr.ReadToEnd());
             }
-        }
-
-        }catch(Exception e){
-            return new Result(Result.RESULT_ERR, 0, e.Message);
         }
     }
 
