3dcfaa4bc0998d5d699e549d71cee19466732edf
Igor <igor@hobdrive.com>
2012-07-24 18:58:53 +0400

    sensor extra interface

---
+++ src/elm/SensorExtra.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace hobd
+{
+
+/// <summary>Sensor interface for extra data retrieval</summary>
+/// <para> Extra data could be of any type, its only sensor own specification should define how to treat it</para>
+public interface SensorExtra
+{
+    /// <summary>Typed extra values from the sensor</summary>
+    object this[string key]{get;}
+
+    /// <summary>List of available keys for extra values</summary>
+    string[] ExtraKeys{get;}
+
+}
+    
+}
