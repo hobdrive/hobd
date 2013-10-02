92a9c50eb4f0ae7d203b8e1eb81a1803386fe017
dragomirg <dragomirg@abv.bg>
2013-10-02 21:37:48 +0300

    Update ShiftPositionSensor.cs

--- src/elm/common/ShiftPositionSensor.cs
+++ src/elm/common/ShiftPositionSensor.cs
@@ -23,18 +23,36 @@ namespace hobd
         // RPM: Rotation speed of the Engine
         public double GetShiftPos(Sensor speed, Sensor rpm)
         {
+            double dGear;
+            double dTmp;
             if ((false == speed.Valid) || (false == rpm.Valid))
             {
-                return Double.PositiveInfinity;
+                return 0;
             }
             if (speed.Value <= 0 || rpm.Value <= 0 || WheelCirc <= 0)
             {
-                return Double.PositiveInfinity;
+                return 0;
+            }
+            
+            dTmp = ((rpm.Value / 60) / (speed.Value * 1000 / 3600 / WheelCirc)) / mainGear;
+            dTmp = Math.Round(dTmp, 2);
+            if (dTmp < 0.94)
+            {
+                dGear = 4;
+            }
+            else if ((dTmp >= 0.94) && (dTmp < 1.87))
+            {
+                dGear = 3;
+            }
+            else if ((dTmp >= 1.87) && (dTmp < 2.49))
+            {
+                dGear = 2;
             }
             else
             {
-                return ((rpm.Value / 60) / (speed.Value * 1000 / 3600 / WheelCirc)) / mainGear;
+                dGear = 1;
             }
+            return dGear;
         }
 
         public double WheelCircleLength
